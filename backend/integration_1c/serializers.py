7936e9e07ac05bf53b6fbecffd36d7386abe38ec
devbelek <belekasrarov10@gmail.com>
2025-01-09 17:11:17 +0600

    dfdfdf

--- backend/integration_1c/serializers.py
+++ backend/integration_1c/serializers.py
@@ -29,7 +29,7 @@ class Product1CSerializer(serializers.ModelSerializer):
             try:
                 # Обновляем существующий товар в основном каталоге
                 main_product.name_en = name_en
-                main_product.name = validated_data['name']  # Обновляем name
+                main_product.name = validated_data['name']
                 main_product.price = price
                 main_product.status = status
 
@@ -43,11 +43,13 @@ class Product1CSerializer(serializers.ModelSerializer):
                 # Создаем временную запись в 1С для логирования
                 instance = Product1C.objects.create(
                     name_en=name_en,
-                    name=validated_data['name'],  # Добавляем name
+                    name=validated_data['name'],
                     vendor_code=vendor_code,
                     price=price,
                     status=status,
-                    is_variation=is_variation
+                    is_variation=is_variation,
+                    is_published=False,
+                    updated_at=None  # Это поле автоматически заполнится
                 )
 
                 if is_variation and base_product_code:
@@ -85,10 +87,11 @@ class Product1CSerializer(serializers.ModelSerializer):
             }
 
             instance.name_en = name_en
-            instance.name = validated_data['name']  # Обновляем name
+            instance.name = validated_data['name']
             instance.price = price
             instance.status = status
             instance.is_variation = is_variation
+            instance.is_published = False
 
             if is_variation and base_product_code:
                 base_product_1c = Product1C.objects.filter(vendor_code=base_product_code).first()
@@ -115,12 +118,14 @@ class Product1CSerializer(serializers.ModelSerializer):
 
                 instance = Product1C.objects.create(
                     name_en=name_en,
-                    name=validated_data['name'],  # Добавляем name
+                    name=validated_data['name'],
                     vendor_code=vendor_code,
                     price=price,
                     status=status,
                     is_variation=is_variation,
-                    base_product=base_product_1c
+                    base_product=base_product_1c,
+                    is_published=False,
+                    updated_at=None  # Это поле автоматически заполнится
                 )
 
                 SyncLog.objects.create(
