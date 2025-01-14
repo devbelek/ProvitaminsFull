cd0d708d163ca54ce72cf4f41fcbbc6ac4f24891
devbelek <belekasrarov10@gmail.com>
2025-01-14 09:37:05 +0600

    dfdfdf

--- frontend/src/screens/product/main-info.tsx
+++ frontend/src/screens/product/main-info.tsx
@@ -1,5 +1,8 @@
+"use client";
+
 import { Product } from "@/src/api/products";
 import Rating from "@/src/components/shared/rating";
+import { useRouter } from "next/navigation";
 import classNames from "classnames";
 import MainInfoActions from "./main-info-actions";
 
@@ -8,6 +11,12 @@ interface Props {
 }
 
 function ProductMainInfo({ data }: Props) {
+  const router = useRouter();
+
+  const handleVariationClick = (variationId: number) => {
+    router.push(`/products/${variationId}`);
+  };
+
   return (
     <div className="grid gap-3 w-full">
       <h1 className="text-xl font-semibold">{data.name}</h1>
@@ -21,47 +30,34 @@ function ProductMainInfo({ data }: Props) {
         </div>
       </div>
       <div className="grid gap-3">
-        {
-          <div className="flex gap-3.5 items-center">
-            <p className="font-medium">Бренд</p>
-            <p className="text-[#808080]">{data.brand_name}</p>
-          </div>
-        }
-        {
-          <div className="flex gap-3.5 items-center">
-            <p className="font-medium">Страна производства</p>
-            <p className="text-[#808080]">{data.country_name}</p>
-          </div>
-        }
-        {
-          <div className="flex gap-3.5 items-center">
-            <p className="font-medium">Форма продукта</p>
-            <p className="text-[#808080]">{data.form_name}</p>
-          </div>
-        }
-
-        {
-          <div className="flex gap-3.5 items-center">
-            <p className="font-medium">Бренд</p>
-            <p className="text-[#808080]">{data.brand_name}</p>
-          </div>
-        }
+        <div className="flex gap-3.5 items-center">
+          <p className="font-medium">Бренд</p>
+          <p className="text-[#808080]">{data.brand_name}</p>
+        </div>
+        <div className="flex gap-3.5 items-center">
+          <p className="font-medium">Страна производства</p>
+          <p className="text-[#808080]">{data.country_name}</p>
+        </div>
+        <div className="flex gap-3.5 items-center">
+          <p className="font-medium">Форма продукта</p>
+          <p className="text-[#808080]">{data.form_name}</p>
+        </div>
+        <div className="flex gap-3.5 items-center">
+          <p className="font-medium">Бренд</p>
+          <p className="text-[#808080]">{data.brand_name}</p>
+        </div>
 
         <div className="flex flex-col gap-3.5">
           {data.variations.current.flavor && (
             <div>
               <div className="flex gap-3.5 pt-3.5 items-center border-t">
                 <p className="font-medium">Вкус</p>
-                <p className="text-[#808080]">
-                  {data.variations.current.flavor}
-                </p>
+                <p className="text-[#808080]">{data.variations.current.flavor}</p>
               </div>
               <div className="flex gap-3.5 mt-1">
-                <a
+                <button
                   className={`font-normal p-3 border transition-all duration-200 rounded-[8px] relative ${"border-[#2CA89A] cursor-default border-2"} ${
-                    !data.variations.current.in_stock
-                      ? "bg-[#F6F6F6] text-[#808080]"
-                      : ""
+                    !data.variations.current.in_stock ? "bg-[#F6F6F6] text-[#808080]" : ""
                   }`}
                 >
                   {data.variations.current.flavor}
@@ -74,23 +70,20 @@ function ProductMainInfo({ data }: Props) {
                         top: 0,
                         width: "100%",
                         height: "100%",
-                        background:
-                          "linear-gradient(to bottom right,#0000 48%, #E6E6E6 ,#0000 52%)",
+                        background: "linear-gradient(to bottom right,#0000 48%, #E6E6E6 ,#0000 52%)",
                       }}
                     ></span>
                   )}
-                </a>
+                </button>
                 {data.variations?.available_variations?.map((variation) => (
-                  <a
+                  <button
                     key={variation.id}
-                    href={process.env.DOMAIN_NAME + "/products/" + variation.id}
+                    onClick={() => handleVariationClick(variation.id)}
                     className={`font-normal p-3 border transition-all duration-200 rounded-[8px] relative ${
                       variation.flavor !== data.variations.current.flavor
                         ? "border-[#E6E6E6] hover:border-[#2CA89A]"
                         : "border-[#2CA89A] cursor-default border-2"
-                    } ${
-                      !variation.in_stock ? "bg-[#F6F6F6] text-[#808080]" : ""
-                    }`}
+                    } ${!variation.in_stock ? "bg-[#F6F6F6] text-[#808080]" : ""}`}
                   >
                     {variation.flavor}
                     {variation.in_stock || (
@@ -102,12 +95,11 @@ function ProductMainInfo({ data }: Props) {
                           top: 0,
                           width: "100%",
                           height: "100%",
-                          background:
-                            "linear-gradient(to bottom right,#0000 48%, #E6E6E6 ,#0000 52%)",
+                          background: "linear-gradient(to bottom right,#0000 48%, #E6E6E6 ,#0000 52%)",
                         }}
                       ></span>
                     )}
-                  </a>
+                  </button>
                 ))}
               </div>
             </div>
@@ -117,20 +109,15 @@ function ProductMainInfo({ data }: Props) {
             <div>
               <div className="flex gap-3.5 pt-3.5 items-center border-t">
                 <p className="font-medium">Дозировка</p>
-                <p className="text-[#808080]">
-                  {data.variations.current.dosage} 
-                </p>
+                <p className="text-[#808080]">{data.variations.current.dosage}</p>
               </div>
-
               <div className="flex gap-3.5 mt-1">
-              <a
+                <button
                   className={`font-normal p-3 border transition-all duration-200 rounded-[8px] relative ${"border-[#2CA89A] cursor-default border-2"} ${
-                    !data.variations.current.in_stock
-                      ? "bg-[#F6F6F6] text-[#808080]"
-                      : ""
+                    !data.variations.current.in_stock ? "bg-[#F6F6F6] text-[#808080]" : ""
                   }`}
                 >
-                  {data.variations.current.dosage} 
+                  {data.variations.current.dosage}
                   {data.variations.current.in_stock || (
                     <span
                       style={{
@@ -140,26 +127,22 @@ function ProductMainInfo({ data }: Props) {
                         top: 0,
                         width: "100%",
                         height: "100%",
-                        background:
-                          "linear-gradient(to bottom right,#0000 48%, #E6E6E6 ,#0000 52%)",
+                        background: "linear-gradient(to bottom right,#0000 48%, #E6E6E6 ,#0000 52%)",
                       }}
                     ></span>
                   )}
-                </a>
+                </button>
                 {data.variations?.available_variations?.map((variation) => (
-                  <a
+                  <button
                     key={variation.id}
-                    href={process.env.DOMAIN_NAME + "/products/" + variation.id}
+                    onClick={() => handleVariationClick(variation.id)}
                     className={`font-normal p-3 border transition-all duration-200 rounded-[8px] relative ${
                       variation.id !== data.variations.current.id
                         ? "border-[#E6E6E6] hover:border-[#2CA89A]"
                         : "border-[#2CA89A] cursor-default border-2"
-                    } 
-                    ${
-                      !variation.in_stock ? "bg-[#F6F6F6] text-[#808080]" : ""
-                    }`}
+                    } ${!variation.in_stock ? "bg-[#F6F6F6] text-[#808080]" : ""}`}
                   >
-                    {variation.dosage} 
+                    {variation.dosage}
                     {variation.in_stock || (
                       <span
                         style={{
@@ -169,12 +152,11 @@ function ProductMainInfo({ data }: Props) {
                           top: 0,
                           width: "100%",
                           height: "100%",
-                          background:
-                            "linear-gradient(to bottom right,#0000 47%, #E6E6E6 ,#0000 53%)",
+                          background: "linear-gradient(to bottom right,#0000 47%, #E6E6E6 ,#0000 53%)",
                         }}
                       ></span>
                     )}
-                  </a>
+                  </button>
                 ))}
               </div>
             </div>
@@ -184,20 +166,15 @@ function ProductMainInfo({ data }: Props) {
             <div>
               <div className="flex gap-3.5 pt-3.5 items-center border-t">
                 <p className="font-medium">Количество в упаковке</p>
-                <p className="text-[#808080]">
-                  {data.variations.current.quantity} 
-                </p>
+                <p className="text-[#808080]">{data.variations.current.quantity}</p>
               </div>
-
               <div className="flex gap-3.5 mt-1 overflow-auto">
-              <a
+                <button
                   className={`font-normal text-center p-3 border transition-all duration-200 rounded-[8px] relative ${"border-[#2CA89A] cursor-default border-2"} ${
-                    !data.variations.current.in_stock
-                      ? "bg-[#F6F6F6] text-[#808080]"
-                      : ""
+                    !data.variations.current.in_stock ? "bg-[#F6F6F6] text-[#808080]" : ""
                   }`}
                 >
-                  {data.variations.current.quantity} 
+                  {data.variations.current.quantity}
                   <span className="block font-medium mt-1">{data.variations.current.price}с</span>
                   {data.variations.current.in_stock || (
                     <span
@@ -208,29 +185,24 @@ function ProductMainInfo({ data }: Props) {
                         top: 0,
                         width: "100%",
                         height: "100%",
-                        background:
-                          "linear-gradient(to bottom right,#0000 48%, #E6E6E6 ,#0000 52%)",
+                        background: "linear-gradient(to bottom right,#0000 48%, #E6E6E6 ,#0000 52%)",
                       }}
                     ></span>
                   )}
-                </a>
+                </button>
                 {data.variations?.available_variations
                   ?.sort((a, b) => a.quantity - b.quantity)
                   .map((variation) => (
-                    <a
+                    <button
                       key={variation.id}
-                      href={
-                        process.env.DOMAIN_NAME + "/products/" + variation.id
-                      }
+                      onClick={() => handleVariationClick(variation.id)}
                       className={`font-normal p-3 border transition-all duration-200 rounded-[8px] relative text-center ${
                         variation.quantity !== data.variations.current.quantity
                           ? "border-[#E6E6E6] hover:border-[#2CA89A]"
                           : "border-[#2CA89A] cursor-default border-2"
-                      } ${
-                        !variation.in_stock ? "bg-[#F6F6F6] text-[#808080]" : ""
-                      }`}
+                      } ${!variation.in_stock ? "bg-[#F6F6F6] text-[#808080]" : ""}`}
                     >
-                      {variation.quantity} 
+                      {variation.quantity}
                       <span className="block font-medium mt-1">{variation.price}с</span>
                       {variation.in_stock || (
                         <span
@@ -241,12 +213,11 @@ function ProductMainInfo({ data }: Props) {
                             top: 0,
                             width: "100%",
                             height: "100%",
-                            background:
-                              "linear-gradient(to bottom right,#0000 47%, #E6E6E6 ,#0000 53%)",
+                            background: "linear-gradient(to bottom right,#0000 47%, #E6E6E6 ,#0000 53%)",
                           }}
                         ></span>
                       )}
-                    </a>
+                    </button>
                   ))}
               </div>
             </div>
@@ -256,12 +227,8 @@ function ProductMainInfo({ data }: Props) {
 
       <div className="flex justify-between items-center gap-4">
         <div className="flex gap-2.5">
-          <p className="text-xl font-semibold">
-            {data.sale_price || data.price}с
-          </p>
-          {data.sale_price && (
-            <p className="text-[20px] leading-6 line-through">{data.price}с</p>
-          )}
+          <p className="text-xl font-semibold">{data.sale_price || data.price}с</p>
+          {data.sale_price && <p className="text-[20px] leading-6 line-through">{data.price}с</p>}
         </div>
         <MainInfoActions />
       </div>
@@ -277,15 +244,8 @@ interface TagProps {
 function Tag({ isHit, isSale }: TagProps) {
   return (
     (isHit || isSale) && (
-      <div
-        className={classNames(
-          "py-1 px-5 rounded",
-          isHit ? "bg-secondary" : "bg-[#E51616]"
-        )}
-      >
-        <span className="font-semibold text-white">
-          {isHit ? "Хит" : "Акция"}
-        </span>
+      <div className={classNames("py-1 px-5 rounded", isHit ? "bg-secondary" : "bg-[#E51616]")}>
+        <span className="font-semibold text-white">{isHit ? "Хит" : "Акция"}</span>
       </div>
     )
   );
